b4e0987a458a65b2c6e8b5a324344d02862da6c6
PuPu-KT <jkenyou@yahoo.com.sg>
2017-07-08 13:06:31 +0800

    files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { Products, Cart } from './containers';
@@ -6,6 +7,13 @@ import { createStore } from 'redux';
 import reducers from './reducers/index';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Components/App/App';
+import registerServiceWorker from './registerServiceWorker';
+>>>>>>> 8936ce74aa2992d75a313d85953f58e2105f391c
 
 const store = createStore(reducers);
 ReactDom.render(
